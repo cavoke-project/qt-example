60e58ee56dce0e582b03cc82d7d72c176c4b9cc0
Mark <markthehopeful@gmail.com>
2022-02-01 00:06:11 +0300

    Excluded qml file from build

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,6 +19,6 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     QQuickView *view = new QQuickView;
-    view->setSource(QUrl("qrc:/tictactoe/qml/tic-tac-toe.qml"));
+    view->setSource(QUrl("/home/mark/CLionProjects/qt-example/qml/tic-tac-toe.qml"));
     view->show();
 }
